556f3a08fde4ee82310959c9b404b6ac3f8b7536
JacobBrown <jcb99mt@byui.edu>
2025-03-25 23:47:26 -0600

    blog cache

---
+++ pages/1_Weekly Blog.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("My Blog in Streamlit")
+
+blog_url = "https://jacobbrown6908.github.io/CNN-Facial_Recognistion-JacobBrown/"
+st.components.v1.iframe(blog_url, height=800, scrolling=True)
